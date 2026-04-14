77b2242057775a0b4ed66d9ccf235a7ecf6748a4
Dina Salama <dinassm04@gmail.com>
2026-04-14 03:30:57 +0200

    delete not needed folder in backend

--- backend - v2 register all senarios/models/admin.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String
-from database import Base
-
-class Admin(Base):
-    __tablename__ = "admins"
-
-    user_id = Column(String(36), primary_key=True)
-    full_name = Column(String(255))
--- backend -v1 correct apis , collection and automatic testing/models/student.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-from database import Base
-
-class Student(Base):
-    __tablename__ = "students"
-
-    user_id = Column(String(36), primary_key=True)
-    university = Column(String(255))
-    college = Column(String(255))
